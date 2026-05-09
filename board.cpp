4674bc59c8a008de6feabb849995f5290cd17932
Salma-Khaled <salmaakh000@gmail.com>
2026-05-09 20:05:37 +0300

    update

--- board.cpp
+++ board.cpp
@@ -6,21 +6,16 @@ char board[4][4];
 
 void initializeBoard() {
     for(int i=0; i<4; i++)
-        for(int j=0; j<4; j++) board[i][j] = ' ';
+        for(int j=0; j<4; j++) board[i][j] = '  ';
 }
 
 void drawBoard() {
     for(int i=0; i<4; i++) {
         cout << "| " ;
         for( int j=0; j<4; j++) {
-            cout << board[i][j] << "|";
+            cout << board[i][j] << "| ";
         }
-       cout << endl;
-        if (i < 3) {
-            cout << "---|---|---|---" << endl;
-        }
-    }
-    cout << endl;
-    }
-
-
+       cout << endl << "---+--+--+---" << endl;
+        
+}
+}
