a8349321f27eca205771fa566bd4059f2b1e659e
Salma-Khaled <salmaakh000@gmail.com>
2026-05-09 16:20:22 +0300

    Add files via upload

---
+++ WinCheck.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include "board.h"
+#include "player_input.h"
+#include "WinCheck.h"
+using namespace std;
+bool checkWin(char player) {
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j <= 1; j++) { 
+
+            if (board[i][j] == player &&
+                board[i][j+1] == player &&
+                board[i][j+2] == player) {
+                return true;
+            }
+        }
+    }
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j <= 1; j++) {
+            if (board[j][i] == player &&
+                board[j+1][i] == player &&
+                board[j+2][i] == player) {
+                    
+                  return true;
+            } 
+        }
+    }
+ 
+    for (int i = 0; i <= 1; i++) {
+        for (int j = 0; j <= 1; j++) {
+            
+            if ( board[i][j] == player &&
+                 board[i+1][j+1] == player &&
+                 board[i+2][j+2] == player){
+                    return true;
+                 }
+            
+            if (board[i][j+2] == player &&
+                board[i+1][j+1] == player &&
+                board[i+2][j] == player){
+                  
+                    return true;
+                }
+        }
+    }
+
+    return false; 
+}
